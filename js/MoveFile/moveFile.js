5273f533ba268eefc5c8f3e8fb36245f078943f7
Caynan Ramos Silva <82410125+caynansilva@users.noreply.github.com>
2021-11-03 10:47:32 -0300

    Update moveFile.js
    Adicionadas Validações no Arquivo de Mover

--- js/MoveFile/moveFile.js
+++ js/MoveFile/moveFile.js
@@ -1,9 +1,19 @@
-var fs = require('fs')
+function MoveFile(arquivo, oldPath, newPath){
+    var fs = require('fs')
 
-var oldPath = 'C:/Projetos/batch/file.txt'
-var newPath = 'C:/Projetos/batch/move/file.txt'
+    if (fs.existsSync(oldPath) && fs.existsSync(newPath) && fs.existsSync(oldPath + arquivo)) {
 
-fs.rename(oldPath, newPath, function (err) {
-  if (err) throw err
-  console.log('Successfully renamed - AKA moved!')
-})
+        fs.rename(oldPath + arquivo, newPath + arquivo, function (err) {
+            if (err) throw err
+            console.log('Arquivo Movido!');
+        })
+    } else {
+        console.log('Caminho Ou Arquivo Não Existem');
+    }
+}
+
+var arquivo = 'file.txt';
+var oldPath = 'C:/Projetos/batch/';
+var newPath = 'C:/Projetos/batch/move/';
+
+MoveFile(arquivo, oldPath, newPath);
